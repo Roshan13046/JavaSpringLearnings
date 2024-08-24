1daf518ebc16c4291f676c268d1ab4f10d7d087a
Roshan13046 <roshanpk.ruat@gmail.com>
2024-08-24 21:01:01 +0530

    More than one type parameters in Generic Class

---
+++ JavaClassesInDepth/src/GenericClasses/Pair.java
@@ -0,0 +1,44 @@
+package GenericClasses;
+
+public class Pair <K,V>{
+    K key;
+    V value;
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public void put(K key, V value){
+        key = key;
+        value = value;
+    }
+
+    public static void main(String[] args) {
+        Pair<String, Integer> stringCount = new Pair<>();
+        Pair<Integer, Boolean> isIntegerPresent = new Pair<>();
+
+        stringCount.put("Ram", 1);
+        stringCount.put("Laxman",4);
+
+        isIntegerPresent.put(23, Boolean.TRUE);
+        isIntegerPresent.put(124, Boolean.FALSE);
+        isIntegerPresent.put(456, Boolean.FALSE);
+
+    }
+}
+
+
+
+
