265864885a44281ae648f2dab3995cf65fb98349
Roshan13046 <roshanpk.ruat@gmail.com>
2024-08-28 10:17:39 +0530

    Lazy initialization in singleton class

---
+++ JavaClassesInDepth/src/SingletonClass/DBConnectionLazy.java
@@ -0,0 +1,20 @@
+package SingletonClass;
+
+public class DBConnectionLazy {
+//    Adv: Now the instance gets created only if the singleton getInstance() method is called, else it won't
+    //DisAdv: if two threads running in parallel and the object is null, it may create multiple objects still
+    private static DBConnectionLazy dbConnectionLazy = new DBConnectionLazy();
+
+    private DBConnectionLazy(){
+
+    }
+
+    //Lazy initialization
+    public DBConnectionLazy getInstance(){
+        if(dbConnectionLazy == null){
+            dbConnectionLazy = new DBConnectionLazy();
+        }
+        return dbConnectionLazy;
+    }
+
+}
