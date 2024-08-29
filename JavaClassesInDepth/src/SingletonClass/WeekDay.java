372d876e1b37a1acac29de7a1fdd924ba7baca5f
Roshan13046 <roshanpk.ruat@gmail.com>
2024-08-29 09:51:20 +0530

    Enum Singleton class code

---
+++ JavaClassesInDepth/src/SingletonClass/WeekDay.java
@@ -0,0 +1,11 @@
+package SingletonClass;
+
+public enum WeekDay {
+    MONDAY, TUESDAY, WEDNESDAY;
+
+    //Enum class is also a Singleton class
+    //it's constructor is always private and it's constants are static final and cannot be changed
+     WeekDay(){
+
+    }
+}
