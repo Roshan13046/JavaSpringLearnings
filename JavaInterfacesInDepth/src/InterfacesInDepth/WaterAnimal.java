266c550939208382203beb4434afd5557b115612
Roshan13046 <roshanpk.ruat@gmail.com>
2024-09-05 01:12:45 +0530

    Methods and fields in interface

--- JavaInterfacesInDepth/src/InterfacesInDepth/WaterAnimal.java
+++ JavaInterfacesInDepth/src/InterfacesInDepth/WaterAnimal.java
@@ -1,5 +1,10 @@
 package InterfacesInDepth;
 
 public interface WaterAnimal {
-    public void canBreath();
+    //methods cannot be final in interfaces
+     public void canBreath();
+
+     //interfaces fields cannot be private or protected and are by default public static final
+     public static final Integer MAX_LENGTH = 2000;
+     Integer MAX_WIGHT = 9000;
 }
