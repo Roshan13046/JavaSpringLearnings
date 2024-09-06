198e232afafb98311d99b69dd7814aeb2a8f117c
Roshan13046 <roshanpk.ruat@gmail.com>
2024-09-06 10:57:09 +0530

    way 3 to implement the default method in interfaces

--- JavaInterfacesInDepth/src/InterfaceFeaturesInJava8AndJava9/Animal.java
+++ JavaInterfacesInDepth/src/InterfaceFeaturesInJava8AndJava9/Animal.java
@@ -2,6 +2,16 @@ package InterfaceFeaturesInJava8AndJava9;
 
 public interface Animal extends LivingThing{
 
-    //here the default method is declared as abstract method
-    boolean canBreathe();
+    //way 3 to define the default method present in the interfaces
+    default boolean canBreathe(){
+        boolean canBreatheOrNot = LivingThing.super.canBreathe();
+
+        if(canBreatheOrNot){
+            System.out.println("yes it can Breathe");
+        }else{
+            System.out.println("No it cannot Breathe");
+        }
+
+        return canBreatheOrNot;
+    }
 }
--- JavaInterfacesInDepth/src/InterfaceFeaturesInJava8AndJava9/Tiger.java
+++ JavaInterfacesInDepth/src/InterfaceFeaturesInJava8AndJava9/Tiger.java
@@ -2,11 +2,11 @@ package InterfaceFeaturesInJava8AndJava9;
 
 public class Tiger implements Animal{
 
-//way 2 to implement the default method it has to be defined here
-
-    @Override
-    public boolean canBreathe() {
-        //do anything here
-        return true;
-    }
+////way 2 to implement the default method it has to be defined here
+//
+//    @Override
+//    public boolean canBreathe() {
+//        //do anything here
+//        return true;
+//    }
 }
