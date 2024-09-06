e12ae279cb00f88e16f22387cbeffbcae4bf88eb
Roshan13046 <roshanpk.ruat@gmail.com>
2024-09-06 10:53:54 +0530

    way 2 to implement the default method in interfaces

--- JavaInterfacesInDepth/src/InterfaceFeaturesInJava8AndJava9/Animal.java
+++ JavaInterfacesInDepth/src/InterfaceFeaturesInJava8AndJava9/Animal.java
@@ -2,4 +2,6 @@ package InterfaceFeaturesInJava8AndJava9;
 
 public interface Animal extends LivingThing{
 
+    //here the default method is declared as abstract method
+    boolean canBreathe();
 }
--- JavaInterfacesInDepth/src/InterfaceFeaturesInJava8AndJava9/Tiger.java
+++ JavaInterfacesInDepth/src/InterfaceFeaturesInJava8AndJava9/Tiger.java
@@ -2,4 +2,11 @@ package InterfaceFeaturesInJava8AndJava9;
 
 public class Tiger implements Animal{
 
+//way 2 to implement the default method it has to be defined here
+
+    @Override
+    public boolean canBreathe() {
+        //do anything here
+        return true;
+    }
 }
