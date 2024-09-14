50862976847742e3cd60e45f5a4596f7e902a979
Roshan13046 <roshanpk.ruat@gmail.com>
2024-09-15 00:59:40 +0530

    Use case 2: Non functional Interface exending a Functional Interface

---
+++ JavaInterfacesInDepth/src/InterfaceFeaturesInJava8AndJava9/ScenariosOfFunctionalInterfacesExtending/Hen.java
@@ -0,0 +1,6 @@
+package InterfaceFeaturesInJava8AndJava9.ScenariosOfFunctionalInterfacesExtending;
+
+//Non functional Interface extending a Functional Interface
+public interface Hen extends Bird{
+    public void canRun();
+}
---
+++ JavaInterfacesInDepth/src/InterfaceFeaturesInJava8AndJava9/ScenariosOfFunctionalInterfacesExtending/Main.java
@@ -0,0 +1,7 @@
+package InterfaceFeaturesInJava8AndJava9.ScenariosOfFunctionalInterfacesExtending;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
