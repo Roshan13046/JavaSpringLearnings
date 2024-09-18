e326ec426a2e9e4550f4f655c51a5179e318c765
Roshan13046 <roshanpk.ruat@gmail.com>
2024-09-18 10:53:15 +0530

    Reflect java class and access it's metadata

--- ReflectionInJava/src/Bird.java
+++ ReflectionInJava/src/Bird.java
@@ -1,2 +1,12 @@
 public class Bird {
+    public String name;
+    private Boolean canRun;
+
+    public void canFly(){
+        System.out.println("Can Fly");
+    }
+
+    public void canEat(){
+        System.out.println("Can Eat");
+    }
 }
--- ReflectionInJava/src/Main.java
+++ ReflectionInJava/src/Main.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Modifier;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -11,6 +13,13 @@ public class Main {
             //way 3 using .getClass()
             Bird birdObj = new Bird();
             Class birdClass3 = birdObj.getClass();
+
+
+            //How to handle reflection of classes
+            System.out.println(birdClass2.getName());
+            System.out.println(Modifier.toString(birdClass2.getModifiers()));
+            System.out.println(birdClass2.getFields());
+            System.out.println(birdClass2.getDeclaredFields());
 
         }catch (Exception e){
             System.out.println(e);
