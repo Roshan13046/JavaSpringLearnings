5c628e35e29c975d0d118a63ff78fe7c1d4e265f
Roshan13046 <roshanpk.ruat@gmail.com>
2024-09-18 10:02:18 +0530

    ways to get the Class object

--- ReflectionInJava/src/Main.java
+++ ReflectionInJava/src/Main.java
@@ -2,7 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        try{
+            //getting the object of the class for getting the metadata information of the bird class
+            //way 1 using Class.formName()
+            Class birdClass = Class.forName("Bird");
+            //way 2 using .class
+            Class birdClass2 = Bird.class;
+            //way 3 using .getClass()
+            Bird birdObj = new Bird();
+            Class birdClass3 = birdObj.getClass();
 
+        }catch (Exception e){
+            System.out.println(e);
+        }
 
     }
 }
