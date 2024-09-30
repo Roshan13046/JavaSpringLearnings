d83e8ed1af0f51bfc78368a1fd3d08dda896f368
Roshan13046 <roshanpk.ruat@gmail.com>
2024-09-30 09:37:39 +0530

    @Target annotation in java

---
+++ JavaAnnotations/src/AnnotationsUsedOverAnotherAnnotation/Bird.java
@@ -0,0 +1,7 @@
+package AnnotationsUsedOverAnotherAnnotation;
+
+@FunctionalInterface
+public interface Bird {
+    public void canFly();
+    //public void canRun(List<Integer> ...heights);
+}
---
+++ JavaAnnotations/src/AnnotationsUsedOverAnotherAnnotation/Peacock.java
@@ -0,0 +1,8 @@
+package AnnotationsUsedOverAnotherAnnotation;
+
+public class Peacock implements Bird{
+    @Override//check the java doc of @Override annotation to get info about the @Target annotation
+    public void canFly() {
+        System.out.println("Yes, Peacock can low fly");
+    }
+}
