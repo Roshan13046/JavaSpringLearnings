11b667ced56895270414d5472144496b7d10077d
Roshan13046 <roshanpk.ruat@gmail.com>
2024-10-01 10:59:06 +0530

    RetentionPolicy.CLASS

--- JavaAnnotations/src/AnnotationsUsedOverAnotherAnnotation/Main.java
+++ JavaAnnotations/src/AnnotationsUsedOverAnotherAnnotation/Main.java
@@ -6,6 +6,6 @@ public class Main {
         peakcock.canFly();
 
         //using reflection concept to check Retention annotation type
-        System.out.println(peakcock.getClass().getAnnotation(MyCustomAnnotation.class)); // will give null as Retention Policy is .SOURCE
+        System.out.println(peakcock.getClass().getAnnotation(MyCustomAnnotation.class)); // will give null as Retention Policy is now .CLASS
     }
 }
