3f4b4b4b9cd55361649b3be2cceef4f2723c5257
Roshan13046 <roshanpk.ruat@gmail.com>
2024-10-01 11:01:30 +0530

    RetentionPolicy.RUNTIME with custom Annotation

--- JavaAnnotations/src/AnnotationsUsedOverAnotherAnnotation/Main.java
+++ JavaAnnotations/src/AnnotationsUsedOverAnotherAnnotation/Main.java
@@ -6,6 +6,7 @@ public class Main {
         peakcock.canFly();
 
         //using reflection concept to check Retention annotation type
-        System.out.println(peakcock.getClass().getAnnotation(MyCustomAnnotation.class)); // will give null as Retention Policy is now .CLASS
+        System.out.println(peakcock.getClass().getAnnotation(MyCustomAnnotation.class)); // will give AnnotationsUsedOverAnotherAnnotation.MyCustomAnnotation()
+        // as Retention Policy is now .RUNTIME so reflection can find the value during RUNTIME
     }
 }
