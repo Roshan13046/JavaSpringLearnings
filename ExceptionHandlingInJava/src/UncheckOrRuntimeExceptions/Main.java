0a25982567c86ed6133f6c135c0fd781be26c758
Roshan13046 <roshanpk.ruat@gmail.com>
2024-10-22 10:56:58 +0530

    ArithmeticException Runtime Exception

--- ExceptionHandlingInJava/src/UncheckOrRuntimeExceptions/Main.java
+++ ExceptionHandlingInJava/src/UncheckOrRuntimeExceptions/Main.java
@@ -2,8 +2,13 @@ package UncheckOrRuntimeExceptions;
 
 public class Main {
     public static void main(String[] args) {
+        int val = 5/0; //ArithmeticException
+        System.out.println(val);
+
         Object obj = 0;
         //ClassCastException
         System.out.println((String) obj);
+
+
     }
 }
