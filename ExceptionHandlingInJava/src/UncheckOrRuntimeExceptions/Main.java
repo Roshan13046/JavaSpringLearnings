9ba59d0f953907d478a9b0b64832e8374be09e23
Roshan13046 <roshanpk.ruat@gmail.com>
2024-10-22 11:05:46 +0530

    IndexOutOfBoundExceptions and it's types

--- ExceptionHandlingInJava/src/UncheckOrRuntimeExceptions/Main.java
+++ ExceptionHandlingInJava/src/UncheckOrRuntimeExceptions/Main.java
@@ -2,6 +2,17 @@ package UncheckOrRuntimeExceptions;
 
 public class Main {
     public static void main(String[] args) {
+        //IndexOutOfBoundException
+
+        //StringIndexOutOfBoundsException
+        String str = "ROSHAN";
+        System.out.println(str.charAt(6));
+
+        //ArrayIndexOutOfBoundsException
+        int [] arr = new int[3];
+        System.out.println(arr[5]);
+
+
         int val = 5/0; //ArithmeticException
         System.out.println(val);
 
