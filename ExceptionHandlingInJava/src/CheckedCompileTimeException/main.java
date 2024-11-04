829ca11305a7ab57d601f181204b81f9252f4951
Roshan13046 <roshanpk.ruat@gmail.com>
2024-11-04 10:42:30 +0530

    Handling compile time exception using throws keyword

--- ExceptionHandlingInJava/src/CheckedCompileTimeException/main.java
+++ ExceptionHandlingInJava/src/CheckedCompileTimeException/main.java
@@ -1,11 +1,12 @@
 package CheckedCompileTimeException;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         method1();
     }
 
-    public static void method1(){
+    //throws keyword asks the caller method to handle the exception
+    public static void method1() throws ClassNotFoundException{
         throw new ClassNotFoundException();
     }
 }
